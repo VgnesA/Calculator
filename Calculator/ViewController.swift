0691316837d7614f529f620e13403bb88a3a25d8
Alle <vigneshalle8333@gmail.com>
2024-05-18 14:47:02 -0400

    Calculator Updated

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -14,6 +14,78 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBOutlet weak var Calculator: UILabel!
+    
+    @IBOutlet weak var Textfield: UITextField!
+    
+    @IBOutlet weak var Result: UILabel!
+    
+    @IBAction func clear(_ sender: UIButton) {
+    }
+    
+    @IBAction func plusorminus(_ sender: UIButton) {
+    }
+    
+    @IBAction func percent(_ sender: UIButton) {
+    }
+    
+    @IBAction func division(_ sender: UIButton) {
+    }
+    
+    @IBAction func seven(_ sender: UIButton) {
+    }
+    
+    @IBAction func eight(_ sender: UIButton) {
+    }
+    
+    @IBAction func nine(_ sender: UIButton) {
+    }
+    
+    @IBAction func multi(_ sender: UIButton) {
+    }
+    
+    @IBAction func four(_ sender: UIButton) {
+    }
+    
+    @IBAction func five(_ sender: UIButton) {
+    }
+    
+    @IBAction func six(_ sender: UIButton) {
+    }
+    
+    @IBAction func sub(_ sender: UIButton) {
+    }
+    
+    @IBAction func one(_ sender: UIButton) {
+    }
+    
+    @IBAction func two(_ sender: UIButton) {
+    }
+    
+    @IBAction func three(_ sender: UIButton) {
+    }
+    
+    @IBAction func sum(_ sender: UIButton) {
+    }
+    
+    @IBAction func zero(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func decimal(_ sender: UIButton) {
+    }
+    
+    @IBAction func equal(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
 
